3dd0b6dc4c04784b0d0f8ed4ff01e19bd9ac9c5c
viktorsirotin <vxod@sirotin.eu>
2022-10-19 10:43:37 +0200

    Copyright added in some files

--- src/main/kotlin/eu/sirotin/siunits/core/DimensionSpecification.kt
+++ src/main/kotlin/eu/sirotin/siunits/core/DimensionSpecification.kt
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2022.  Viktor Sirotin
+ *
+ *  *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  *  furnished to do so, subject to the following conditions:
+ *  *  This Copyright header should still in file if you use it file without changes or
+ *  * with changes or if you copy essential part of this file in some new file.
+ *  *  *
+ *  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  * THE SOFTWARE.
+ *
+ */
+
 package eu.sirotin.siunits.core
 
 data class DimensionSpecification<T : TermUnit>(val unitName: String,
--- src/main/kotlin/eu/sirotin/siunits/core/TermUnit.kt
+++ src/main/kotlin/eu/sirotin/siunits/core/TermUnit.kt
@@ -7,8 +7,9 @@
  *   * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *   * copies of the Software, and to permit persons to whom the Software is
  *   * furnished to do so, subject to the following conditions:
- *   * This Copyright header should still in file if you use it file without changes or with changes or if you copy essential part of this file in some new file.
- *  *
+ *   * This Copyright header should still in file if you use it file without changes or
+ *   * with changes or if you copy essential part of this file in some new file.
+ *   *
  *   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
