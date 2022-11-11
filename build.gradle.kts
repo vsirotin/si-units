d331eb25a306773af74201807afb3a2612b241e9
viktorsirotin <vxod@sirotin.eu>
2022-11-11 15:15:47 +0100

    Installation in local maven directory added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,44 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.bundling.Jar
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    id("java-library")
+    id("maven-publish")
 }
 
 group = "eu.sirotin"
-version = "1.0-SNAPSHOT"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group as String?
+            artifactId = "su-unit"
+            version = project.version as String?
+
+            from(components["java"])
+        }
+    }
+}
+
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+tasks.jar {
+    manifest {
+        attributes(mapOf("Implementation-Title" to project.name,
+            "Implementation-Version" to project.version))
+    }
+
+    exclude("eu/sirotin/generator/**")
+    from("README.md")
+
 }
 
 tasks.test {
