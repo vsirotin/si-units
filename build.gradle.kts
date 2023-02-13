e7bca25089be2acc81ffc4199f3f848960593a08
Michal Harakal <michal.harakal@googlemail.com>
2023-02-13 22:58:32 +0100

    use newer version of publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,9 @@
 group = "eu.sirotin.kotunil"
 version = "3.0.0"
 
-buildscript {
-    dependencies {
-            classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
-            classpath("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
-    }
-
-    repositories {
-        mavenCentral()
-        google()
-        gradlePluginPortal()
-    }
+plugins {
+    kotlin("multiplatform") version "1.8.10" apply false
+    id("com.vanniktech.maven.publish") version "0.24.0"
 }
 
 
