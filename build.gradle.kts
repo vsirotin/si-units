b69a93d680d914996373f5ee15e0bdf72be9006a
Michal Harakal <michal.harakal@googlemail.com>
2023-03-30 22:28:36 +0200

    Update maven release plugin to the newer version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ group = "eu.sirotin.kotunil"
 
 plugins {
     kotlin("multiplatform") version "1.8.10" apply false
-    id("com.vanniktech.maven.publish") version "0.24.0"
+    id("com.vanniktech.maven.publish") version "0.25.1"
     id("org.jetbrains.dokka")
 }
 
