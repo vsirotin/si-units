e8304d144b72a3a373ca1bf6c3704d2b7b491764
Michal Harakal <michal.harakal@googlemail.com>
2023-03-30 22:27:49 +0200

    Update settings.gradle.kts
    Update release plugin to newer version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 
     plugins {
         kotlin("multiplatform") version "1.8.10" apply false
-        id("com.vanniktech.maven.publish") version "0.24.0" apply false
+        id("com.vanniktech.maven.publish") version "0.25.1" apply false
         id("com.vanniktech.maven.publish")
         id("org.jetbrains.dokka") version "1.7.20"
     }
