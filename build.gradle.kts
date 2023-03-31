af6ceb8b81cfff06d38d5f125af03e03f663c086
Michal Harakal <michal.harakal@googlemail.com>
2023-03-31 12:13:48 +0200

    Update build.gradle.kts
    Remove publish plugin from root to avoid error on module without publications.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ group = "eu.sirotin.kotunil"
 
 plugins {
     kotlin("multiplatform") version "1.8.10" apply false
-    id("com.vanniktech.maven.publish") version "0.25.1"
     id("org.jetbrains.dokka") version "1.7.20"
 }
 
