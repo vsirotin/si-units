f4cd73d57544083b828039ecc53151f1c42cd4ff
viktorsirotin <vxod@sirotin.eu>
2023-04-26 11:58:29 +0200

    JS NPM 0.0.2 version completed. Related To #39.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,7 @@ rootProject.name = "si-units"
 
 include(":kotunil")
 include(":kotunil-generators")
+include(":js-lib")
 include(":apps:jvm:kotlin-console")
 include(":apps:jvm:java-console")
 include(":apps:web_app_js")
