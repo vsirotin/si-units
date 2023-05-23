7d3e4fbfafb7203fc0c24bc7b8af5112927b6db9
viktorsirotin <vxod@sirotin.eu>
2023-05-23 14:36:22 +0200

    Build for project web_app_js updated.

--- apps/web_app_js/build.gradle.kts
+++ apps/web_app_js/build.gradle.kts
@@ -2,6 +2,15 @@
 group = "eu.sirotin.kotunil.app.js"
 version = "4.1.1"
 
+plugins {
+    id("java") //To enable project dependencies
+}
+
+dependencies {
+    implementation(project(":kotunil"))
+    implementation(project(":js-lib"))
+}
+
 tasks.register("npmInstall") {
     doLast {
         exec {
