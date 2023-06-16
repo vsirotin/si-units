b117e005fd920b081f4b5c189cf1ff481e2c5d90
viktorsirotin <vxod@sirotin.eu>
2023-06-16 11:34:29 +0200

    Update build process by web_app_js

--- apps/web_app_js/build.gradle.kts
+++ apps/web_app_js/build.gradle.kts
@@ -2,22 +2,30 @@
 group = "eu.sirotin.kotunil.app.js"
 version = "4.1.1"
 
-plugins {
-    id("java") //To enable project dependencies
-}
 
 dependencies {
+    project(":kotunil-generators")
     project(":kotunil")
     project(":js-lib")
 }
 
-tasks.register("npmInstall") {
+tasks.register("installFromNpmGlobal") {
     doLast {
         exec {
             executable("npm")
             args("i")
         }
-        println("Please see installed KotUniL lib in directory 'apps/web_app_js/node_modules/kotunil-js-lib'")
+        logger.quiet("Please see installed KotUniL lib in directory 'apps/web_app_js/node_modules/kotunil-js-lib'")
+    }
+}
+
+tasks.register("installFromLocalDirectory") {
+    doLast {
+        exec {
+            executable("npm")
+            args("install", "../../js-lib")
+        }
+        logger.quiet("Please see installed KotUniL lib in directory 'apps/web_app_js/node_modules/kotunil-js-lib'")
     }
 }
 
