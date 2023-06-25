538ea866bc4957887abd6eff0a5ccb8067abb4f6
viktorsirotin <vxod@sirotin.eu>
2023-06-25 12:40:40 +0200

    Update build process. Using js-stuff from special directory js_dist

--- js-lib/build.gradle.kts
+++ js-lib/build.gradle.kts
@@ -5,16 +5,13 @@ dependencies {
     project(":kotunil-generators")
 }
 
-val fromDir = "../build/js/packages/si-units-kotunil/kotlin"
-tasks.register<Copy>("copyOwnJsLib") {
+val fromDir = "../kotunil/js-dist"
+tasks.register<Copy>("copyLibs") {
     from(layout.projectDirectory.file("${fromDir}/si-units-kotunil.js"),
-        layout.projectDirectory.file("${fromDir}/si-units-kotunil.js.map"))
-    into(layout.projectDirectory)
-}
-
-tasks.register<Copy>("copyStdJsLib") {
-    from(layout.projectDirectory.file("${fromDir}/kotlin-kotlin-stdlib-js-ir.js"),
-        layout.projectDirectory.file("${fromDir}/kotlin-kotlin-stdlib-js-ir.js.map"))
+        layout.projectDirectory.file("${fromDir}/si-units-kotunil.js.map"),
+        layout.projectDirectory.file("${fromDir}/kotlin-kotlin-stdlib-js-ir.js"),
+        layout.projectDirectory.file("${fromDir}/kotlin-kotlin-stdlib-js-ir.js.map")
+    )
     into(layout.projectDirectory)
 }
 
