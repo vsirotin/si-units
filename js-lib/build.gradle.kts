61dde8c9d2a7313e62d6aeb10197f90ecbaeba8b
viktorsirotin <vxod@sirotin.eu>
2023-10-25 18:40:01 +0200

    Update NPM publish process.

--- js-lib/build.gradle.kts
+++ js-lib/build.gradle.kts
@@ -7,11 +7,26 @@ dependencies {
 
 val fromDir = "../kotunil/js-dist"
 tasks.register<Copy>("copyLibs") {
-    from(layout.projectDirectory.file("${fromDir}/si-units-kotunil.js"),
-        layout.projectDirectory.file("${fromDir}/si-units-kotunil.js.map"),
-        layout.projectDirectory.file("${fromDir}/kotlin-kotlin-stdlib-js-ir.js"),
-        layout.projectDirectory.file("${fromDir}/kotlin-kotlin-stdlib-js-ir.js.map")
+    from(file("${fromDir}/si-units-kotunil.js"),
+        file("${fromDir}/si-units-kotunil.d.ts"),
+        file("${fromDir}/si-units-kotunil.js.map"),
+        file("${fromDir}/kotlin-kotlin-stdlib-js-ir.js"),
+        file("${fromDir}/kotlin-kotlin-stdlib-js-ir.js.map")
     )
-    into(layout.projectDirectory)
+    into("${layout.projectDirectory}/dist1")
 }
+
+tasks.register("installToGlobalMPM") {
+    doLast {
+        exec {
+            executable("npm")
+            args("publish")
+            workingDir("./dist")
+        }
+
+        logger.quiet("Please see installed KotUniL lib in directory 'apps/web_app_js/node_modules/kotunil-js-lib'")
+    }
+}
+
+
 
