a3648c800db0241f8762b4b66cb946aff37d37d0
viktorsirotin <vxod@sirotin.eu>
2023-11-22 10:45:35 +0100

    Part 3 node_ts_app. Tests for special units. Relevant for #42

--- apps/node_ts_app/src/scripts/startConsole.ts
+++ apps/node_ts_app/src/scripts/startConsole.ts
@@ -2,10 +2,12 @@ import {checkObjects, numberChecks, numberErrors, getNumberCheckedObjects} from 
 import {testTutorial} from './tests/TutorialTest'
 import {testBase} from './tests/base/BaseTSTest'
 import {testDerived} from './tests/derived/DerivedTSTest'
+import {testSpecialUnits} from './tests/specialunits/SpecialUnitsTest'
 
 testTutorial();
 testBase();
 testDerived();
+testSpecialUnits();
 
 console.log("Number checks=" + numberChecks )
 console.log("Number tested objects=" + getNumberCheckedObjects() )
